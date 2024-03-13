fa18acec02f6a0eeb5e958fa0fef219ceeae89d7
Hunde-D <hunddh2025@gmail.com>
2024-03-13 20:51:34 +0300

    update the game
    every thing is working the only thing left is the to the player the game is over.

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,60 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import Die from "./Die";
+import { nanoid } from "nanoid";
 
 function App() {
+  const [dice, setDice] = useState(allNewDice);
+
+  function generateNewDie() {
+    return {
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+      id: nanoid(),
+    };
+  }
+
   function allNewDice() {
     const newDice = [];
     for (let i = 0; i < 10; i++) {
-      newDice.push(Math.ceil(Math.random() * 6));
+      newDice.push(generateNewDie());
     }
     return newDice;
   }
-  const [dice, setDice] = useState(allNewDice);
-  // setDice(newDice);
-  function rollDice() {
-    setDice(allNewDice);
+  function holdDice(id) {
+    setDice((oldDice) =>
+      oldDice.map((die) =>
+        die.id === id ? { ...die, isHeld: !die.isHeld } : die
+      )
+    );
   }
 
-  const dieElement = dice.map((die) => <Die value={die} />);
+  // setDice(newDice);
+  function rollDice() {
+    setDice((oldDice) =>
+      oldDice.map((die) => {
+        return die.isHeld ? die : generateNewDie();
+      })
+    );
+  }
+
+  const dieElement = dice.map((die) => (
+    <Die
+      key={die.id}
+      value={die.value}
+      isHeld={die.isHeld}
+      holdDice={() => holdDice(die.id)}
+    />
+  ));
 
   return (
     <main>
+      <h1 className="title">Tenzies</h1>
+      <p className="instructions">
+        Roll until all dice are the same. Click <br />
+        each die to freeze it at its current value
+        <br /> between rolls.
+      </p>
       <div className="dice--container">{dieElement}</div>
       <button className="roll-dice" onClick={rollDice}>
         Roll
