c9b2f339fb589f1af215567d3c2fc222157af6be
Hunde-D <hunddh2025@gmail.com>
2024-03-13 19:06:56 +0300

    create components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
+import Die from "./Die";
 
 function App() {
-  const [] = useState();
+  function allNewDice() {
+    const newDice = [];
+    for (let i = 0; i < 10; i++) {
+      newDice.push(Math.ceil(Math.random() * 6));
+    }
+    return newDice;
+  }
+  const [dice, setDice] = useState(allNewDice);
+  // setDice(newDice);
+  function rollDice() {
+    setDice(allNewDice);
+  }
 
-  return <></>;
+  const dieElement = dice.map((die) => <Die value={die} />);
+
+  return (
+    <main>
+      <div className="dice--container">{dieElement}</div>
+      <button className="roll-dice" onClick={rollDice}>
+        Roll
+      </button>
+    </main>
+  );
 }
 
 export default App;
